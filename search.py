3dce61c24affcc30bc8a2845202debb184c6f220
Gea-Suan Lin <gslin@gslin.org>
2018-06-01 13:09:05 +0800

    Add skeleton.

---
+++ search.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import feedgen.feed
+import html
+import requests
+import sys
+import urllib
+
+def magic(keyword):
+    url = 'https://www.youtube.com/results?sp=CAI%%253D&search_query=%s' % (urllib.parse.quote_plus(keyword))
+
+    r = requests.get(url);
+
+    title = 'YouTube Search - %s' % (keyword)
+
+    feed = feedgen.feed.FeedGenerator()
+    feed.author({'name': 'YouTube Search Feed Generator'})
+    feed.id(url)
+    feed.link(href=url, rel='alternate')
+    feed.title(title)
+
+    # FIXME
+
+    print(str(feed.atom_str(), 'utf-8'))
+
+if __name__ == '__main__':
+    magic(sys.argv[1])
